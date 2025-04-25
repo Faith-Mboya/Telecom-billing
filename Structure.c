06884dd9da5439f06caf6bc267fcfbcdac5b3dc8
Faith-Mboya <faithawinomboya@gmail.com>
2025-04-25 12:25:56 +0300

    Update Structure.c

--- Structure.c
+++ Structure.c
@@ -29,4 +29,15 @@ int isValidPhoneNumber(const char *phone){
     }
     return 1;
 }
+//Function to viewpayment for a customer
+void viewpayment (const char *phoneNumber)
+{
+    for (int i =0; i < customerCount; i++){
+        if (strcmp(customers[i].phoneNumber, phoneNumber)==0){
+            printf("\nTotal Bill for %s: $%.2f\n", customers[i].name, customers[i].totalBill);
+            return;
+        }
+    }
+     printf("\nRecord not found!\n");
+}
  
